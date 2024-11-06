486cdd60005f10f3f0464cc9f79ca9f32685f7f4
W3Router <info@w3airouter.com>
2024-11-06 11:55:24 +0800

    init
    as

---
+++ js/myself.js
@@ -0,0 +1,20 @@
+$("#kefu_tel_num").popover({
+	trigger : 'hover',
+	html : true,
+	content : "<h5 style='padding-top:15px'>18922803474</h5>"
+});
+$("#kefu_qq").popover({
+	trigger : 'hover',
+	html : true,
+	content : "<h5 style='padding-top:15px'>185874212</h5>"
+});
+$("#online_worlds").popover({
+	trigger : 'hover',
+	html : true,
+	content : "<h5>欢迎点我🙃</h5>"
+});
+$("#kefu_weixn").popover({
+	trigger : 'hover',
+	html : true,
+	content : "<img src='images/kefu_weixin.png'>"
+});
